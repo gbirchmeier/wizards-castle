dbc160e85bf30c758872eeea271b5b65efe42848
Grant Birchmeier <gbirchmeier@connamara.com>
2018-04-24 16:13:06 -0500

    fix more cop violations

--- spec/wizards-castle/player_spec.rb
+++ spec/wizards-castle/player_spec.rb
@@ -7,148 +7,148 @@ module WizardsCastle
 
     let(:player) { Player.new }
 
-    context "#gp" do
-      it "access" do
+    context '#gp' do
+      it 'access' do
         expect(player.gp).to eq 60
       end
-      it "alter" do
+      it 'alter' do
         expect(player.gp(+5)).to eq 65
         expect(player.gp(-2)).to eq 63
         expect(player.gp).to eq 63
       end
-      it "can't be less than zero" do
+      it 'cannot be less than zero' do
         expect(player.gp(-100)).to eq 0
       end
     end
 
-    context "attributes" do
-      it "max 18" do
+    context 'attributes' do
+      it 'max 18' do
         player.str(+25)
         player.int(+50)
         player.dex(+19)
-        expect([player.str,player.int,player.dex]).to eq [18,18,18]
+        expect([player.str, player.int, player.dex]).to eq [18, 18, 18]
       end
-      it "min 0" do
+      it 'min 0' do
         player.str(-25)
         player.int(-50)
         player.dex(-19)
-        expect([player.str,player.int,player.dex]).to eq [0,0,0]
+        expect([player.str, player.int, player.dex]).to eq [0, 0, 0]
       end
     end
 
-    it "flares >= 0" do
+    it 'flares >= 0' do
       expect(player.flares(-10000)).to eq 0
     end
 
-    it "#forget_random_room" do
-      player.remember_room(2,2,2)
-      player.remember_room(3,3,3)
-      allow(Castle).to receive(:random_room).and_return [2,2,2]
+    it '#forget_random_room' do
+      player.remember_room(2, 2, 2)
+      player.remember_room(3, 3, 3)
+      allow(Castle).to receive(:random_room).and_return [2, 2, 2]
       player.forget_random_room
-      expect(player.knows_room?(2,2,2)).to eq false
-      expect(player.knows_room?(3,3,3)).to eq true
+      expect(player.knows_room?(2, 2, 2)).to eq false
+      expect(player.knows_room?(3, 3, 3)).to eq true
     end
 
-    context "#set_weapon" do
-      it ":nothing" do
+    context '#set_weapon' do
+      it ':nothing' do
         player.set_weapon(:nothing)
         expect(player.weapon_value).to eq 0
       end
-      it ":dagger" do
+      it ':dagger' do
         player.set_weapon(:dagger)
         expect(player.weapon_value).to eq 1
       end
-      it ":mace" do
+      it ':mace' do
         player.set_weapon(:mace)
         expect(player.weapon_value).to eq 2
       end
-      it ":sword" do
+      it ':sword' do
         player.set_weapon(:sword)
         expect(player.weapon_value).to eq 3
       end
     end
 
-    context "#set_armor" do
-      it ":nothing" do
+    context '#set_armor' do
+      it ':nothing' do
         player.set_armor(:nothing)
         expect(player.armor_value).to eq 0
         expect(player.armor_health).to eq 0
       end
-      it ":leather" do
+      it ':leather' do
         player.set_armor(:leather)
         expect(player.armor_value).to eq 1
         expect(player.armor_health).to eq 7
       end
-      it ":chainmail" do
+      it ':chainmail' do
         player.set_armor(:chainmail)
         expect(player.armor_value).to eq 2
         expect(player.armor_health).to eq 14
       end
-      it ":plate" do
+      it ':plate' do
         player.set_armor(:plate)
         expect(player.armor_value).to eq 3
         expect(player.armor_health).to eq 21
       end
     end
 
-    context "#take_a_hit!" do
+    context '#take_a_hit!' do
       before(:each) do
         player.str(+18)
       end
 
-      it ":nothing" do
+      it ':nothing' do
         player.set_armor(:nothing)
         player.take_a_hit!(5)
         expect(player.str).to eq 13
       end
 
-      context ":plate" do
+      context ':plate' do
         before(:each) do
           player.set_armor(:plate)
         end
-        it "high damage" do
+        it 'high damage' do
           player.take_a_hit!(5)
-          expect(player.armor_health).to eq 18  #lost 3
-          expect(player.str).to eq 16  #lost 2
+          expect(player.armor_health).to eq 18 #lost 3
+          expect(player.str).to eq 16 #lost 2
           expect(player.armor).to eq :plate
         end
-        it "low damage" do
+        it 'low damage' do
           player.take_a_hit!(1)
-          expect(player.armor_health).to eq 20  #lost 1
-          expect(player.str).to eq 18  #no loss
+          expect(player.armor_health).to eq 20 #lost 1
+          expect(player.str).to eq 18 #no loss
           expect(player.armor).to eq :plate
         end
-        it "low damage" do
+        it 'low damage' do
           player.take_a_hit!(1)
-          expect(player.armor_health).to eq 20  #lost 1
-          expect(player.str).to eq 18  #no loss
+          expect(player.armor_health).to eq 20 #lost 1
+          expect(player.str).to eq 18 #no loss
           expect(player.armor).to eq :plate
         end
-        it "destroyed" do
+        it 'destroyed' do
           player.armor_health = 1
           player.take_a_hit!(5)
           expect(player.armor).to eq :nothing
           expect(player.armor_health).to eq 0
           expect(player.armor_value).to eq 0
-          expect(player.str).to eq 16  #armor still blocked 3, player takes 2
+          expect(player.str).to eq 16 #armor still blocked 3, player takes 2
         end
       end
     end
 
-    it "#add_treasure" do
+    it '#add_treasure' do
       expect(player.treasure_count).to eq 0
       player.add_treasure(:opal_eye)
       expect(player.treasure_count).to eq 1
     end
 
-    it "#have_treasure?" do
+    it '#have_treasure?' do
       expect(player.have_treasure?(:opal_eye)).to eq false
       player.add_treasure(:opal_eye)
       expect(player.have_treasure?(:opal_eye)).to eq true
       expect(player.have_treasure?(:norn_stone)).to eq false
     end
 
-    it "#remove_treasure" do
+    it '#remove_treasure' do
       player.add_treasure(:opal_eye)
       expect(player.treasure_count).to eq 1
       player.remove_treasure(:opal_eye)
